32498f318776e3477f1ee17817805fd6fc59d462
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-01-30 17:51:14 +0500

    start

---
+++ main.dart
@@ -0,0 +1,24 @@
+void main() {
+  List<int> ls = [
+    5,
+    8,
+    9,
+    4,
+    2,
+    3
+  ];
+  List<int> ans=[];
+
+int len=ls.length;
+for(int i=0;i<len;i++){
+  int min =ls[0];
+  for(int j in ls){
+    if (min>j){
+      min=j;
+    }
+  }
+  ans.add(min);
+  ls.remove(min);
+}
+  print(ans);
+}
