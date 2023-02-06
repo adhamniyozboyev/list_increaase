50018e7c1cb5b7c9d45363db0a9314c7764316fa
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-02-06 17:50:28 +0500

    kopaytirish

--- indexofoperations.dart
+++ indexofoperations.dart
@@ -1,5 +1,5 @@
 void main() {
-  String txt = '3+4+5-6*5/9';
+  String txt = '3+4+5-6*5*9/12';
   String exp = '/*-+';
   List<int> ind = [];
   for (int i = 0; i < txt.length; i++) {
@@ -19,4 +19,15 @@ void main() {
   // numbers.add(txt.substring(q),);
   numbers.add(num.parse(txt.substring(ind.last+1)));
   print(numbers);
+
+  for(int i=0;i<ind.length;i++){
+    if(txt[ind[i]]=='*'){
+     numbers[i]=numbers[i]*numbers[i+1];
+     numbers.removeAt(i+1);
+     ind.removeAt(i);
+     i--;
+    }
+  }
+  print(numbers);
+  
 }
