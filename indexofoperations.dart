adb6579b067ede193dbfaf280d1682c7ac33fdef
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-02-06 16:47:56 +0500

    go go

--- indexofoperations.dart
+++ indexofoperations.dart
@@ -1,8 +1,10 @@
-int getindexof(int a,int b,int c){
-  return a*b*c;
-}
-
 void main(){
-  print(getindexof(3, 12, 2));
+  String txt='3+4+5-6*5/0';
+  String exp='/*-+';
+ List<int>ind=[];
+ for (int i=0;i<txt.length;i++){
+  if(txt[i]=='*'||txt[i]=='-'||txt[i]=='+'||txt[i]=='/');
+  ind.add(i);
+ }
+ print(ind);
 }
-
