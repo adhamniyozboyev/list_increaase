c146e0aa298c209bbb1730263ea1011d94e3ac49
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-02-07 09:39:29 +0500

    finishing just a hours

--- indexofoperations.dart
+++ indexofoperations.dart
@@ -1,60 +1,55 @@
 void main() {
   String txt = '3+4+5-6*5*9/12';
+  // String txt = '3+4+5-30*9/12';
   String exp = '/*-+';
   List<int> ind = [];
   for (int i = 0; i < txt.length; i++) {
     // if(txt[i]=='*'||txt[i]=='-'||txt[i]=='+'||txt[i]=='/');
     if (exp.contains(txt[i])) {
       ind.add(i);
+    // print(ind);
     }
   }
-
 
   List<num> numbers = [];
   int q = 0;
   for (int i in ind) {
-    numbers.add(num.parse(txt.substring(q,i)));
+    numbers.add(num.parse(txt.substring(q, i)));
     q = i + 1;
+    // print(numbers);
   }
   // numbers.add(txt.substring(q),);
-  numbers.add(num.parse(txt.substring(ind.last+1)));
-print(numbers);
+  numbers.add(num.parse(txt.substring(ind.last + 1)));
+  // print(numbers);
 
-  for(int i=0;i<ind.length;i++){
-    if(txt[ind[i]]=='*'){
-     numbers[i]=numbers[i]*numbers[i+1];
-     numbers.removeAt(i+1);
-     ind.removeAt(i);
-     i--;
-    }
-      else if(txt[ind[i]]=='/'){
-     numbers[i]=numbers[i]/numbers[i+1];
-     numbers.removeAt(i+1);
-     ind.removeAt(i);
-     i--;
-    }
-
-  }
-
-  print(numbers);
-    
-
-      for(int i=0;i<ind.length;i++){
-    if(txt[ind[i]]=='+'){
-     numbers[i]=numbers[i]+numbers[i+1];
-     numbers.removeAt(i+1);
-     ind.removeAt(i);
-     i--;
-    }
-
-     else if(txt[ind[i]]=='-'){
-     numbers[i]=numbers[i]-numbers[i+1];
-     numbers.removeAt(i+1);
-     ind.removeAt(i);
-     i--;
+  for (int i = 0; i < ind.length; i++) {
+    if (txt[ind[i]] == '*') {
+      numbers[i] = numbers[i] * numbers[i + 1];
+      numbers.removeAt(i + 1);
+      ind.removeAt(i);
+      i--;
+    } else if (txt[ind[i]] == '/') {
+      numbers[i] = numbers[i] / numbers[i + 1];
+      numbers.removeAt(i + 1);
+      ind.removeAt(i);
+      i--;
     }
   }
-  print(numbers);
 
-  
+ 
+
+  for (int i = 0; i < ind.length; i++) {
+    if (txt[ind[i]] == '+') {
+      numbers[i] = numbers[i] + numbers[i + 1];
+      numbers.removeAt(i + 1);
+      ind.removeAt(i);
+      i--;
+    } else if (txt[ind[i]] == '-') {
+      numbers[i] = numbers[i] - numbers[i + 1];
+      numbers.removeAt(i + 1);
+      ind.removeAt(i);
+      i--;
+    }
+  }
+  print(numbers[0]);
 }
--- main.dart
+++ main.dart
@@ -1,24 +1,18 @@
 void main() {
-  List<int> ls = [
-    5,
-    8,
-    9,
-    4,
-    2,
-    3
-  ];
-  List<int> ans=[];
-
-int len=ls.length;
-for(int i=0;i<len;i++){
-  int min =ls[0];
-  for(int j in ls){
-    if (min>j){
-      min=j;
+  List<int> ls = [5, 8, 9, 4, 2, 3];
+  List<int> ans = [];
+  int a = 0;
+  int len = ls.length;
+  for (int i = 0; i < len; i++) {
+    int min = ls[0];
+    for (int j in ls) {
+      if (min > j) {
+        min = j;
+      }
+      print(a++);
     }
+    ans.add(min);
+    ls.remove(min);
   }
-  ans.add(min);
-  ls.remove(min);
-}
   print(ans);
 }
